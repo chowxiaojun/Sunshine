5d38fc845e83fc2f16339c5535a178b7320a27d9
xiaojunzhou <chowxiaojun@126.com>
2016-05-03 20:55:39 +0800

    Activated List Item Style

--- app/src/main/java/com/xiroid/sunshine/app/activity/SettingsActivity.java
+++ app/src/main/java/com/xiroid/sunshine/app/activity/SettingsActivity.java
@@ -1,11 +1,13 @@
 package com.xiroid.sunshine.app.activity;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceManager;
+import android.support.annotation.Nullable;
 
 import com.xiroid.sunshine.app.R;
 
@@ -38,6 +40,12 @@ public class SettingsActivity extends PreferenceActivity
                 PreferenceManager
                         .getDefaultSharedPreferences(preference.getContext())
                         .getString(preference.getKey(), ""));
+    }
+
+    @Nullable
+    @Override
+    public Intent getParentActivityIntent() {
+        return super.getParentActivityIntent().addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
     }
 
     @Override
